bcc2449e4ea518d2d2138479ec5f3696849722ba
Raz_dolbai <napersson@mail.ru>
2022-02-13 20:56:27 +0300

    add test see login link

--- test_main_page.py
+++ test_main_page.py
@@ -9,3 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # создаем объект, передаем параметры browser и ссылку
     page.open()  # открываем браузер
     page.go_to_login_page()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # создаем объект, передаем параметры browser и ссылку
+    page.open()  # открываем браузер
+    page.should_be_login_link()
