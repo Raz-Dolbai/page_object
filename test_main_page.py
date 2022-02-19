44844e6af8522cf4a6877c90d35ddb3033dfa653
Raz_dolbai <napersson@mail.ru>
2022-02-19 22:42:07 +0300

    TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -10,26 +10,6 @@ from .pages.locators import MainPageLocators
 
 # запуск теста
 # pytest -v --tb=line --language=en test_main_page.py
-
-@pytest.mark.main_page
-def test_guest_can_go_to_login_page(browser):
-    link = MainPageLocators.LINK
-    page = MainPage(browser, link)  # создаем объект, передаем параметры browser и ссылку
-    page.open()  # открываем браузер
-    page.go_to_login_page()
-    # Реализация перехода на страницу login
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
-@pytest.mark.main_page
-def test_guest_should_see_login_link(browser):
-    link = MainPageLocators.LINK
-    page = MainPage(browser, link)  # создаем объект, передаем параметры browser и ссылку
-    page.open()  # открываем браузер
-    page.should_be_login_link()
-
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = MainPageLocators.LINK
     main_page = MainPage(browser, link)
@@ -40,8 +20,19 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket.should_not_be_items_in_basket()
 
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_should_see_login_link(self, browser):
+        link = MainPageLocators.LINK
+        page = MainPage(browser, link)  # создаем объект, передаем параметры browser и ссылку
+        page.open()  # открываем браузер
+        page.should_be_login_link()
 
-
-
-
-
+    def test_guest_can_go_to_login_page(self, browser):
+        link = MainPageLocators.LINK
+        page = MainPage(browser, link)  # создаем объект, передаем параметры browser и ссылку
+        page.open()  # открываем браузер
+        page.go_to_login_page()
+        # Реализация перехода на страницу login
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
